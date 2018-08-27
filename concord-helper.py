801cb3d573863fd69d90b3a6615018dc2b78d138
Josh Perry <joshperry@outlook.com>
2018-08-27 13:30:15 +0100

    Made the command a more generic for use with systems

--- concord-helper.py
+++ concord-helper.py
@@ -7,8 +7,10 @@ settings_file = "concord-helper.sublime-settings"
 settings = sublime.load_settings(settings_file)
 
 
-class CreateNewComponentCommand(sublime_plugin.WindowCommand):
+class CreateNewEcsFileCommand(sublime_plugin.WindowCommand):
     def run(self):
+        window = self.window
+
         language = settings.get("language")
         base = os.path.dirname(os.path.realpath(__file__))
         self.templates_path = os.path.join(base, "templates", language)
@@ -19,28 +21,44 @@ class CreateNewComponentCommand(sublime_plugin.WindowCommand):
         f = "system.{0}".format(language)
         self.system_template = os.path.join(self.templates_path, f)
 
-        project_path = self.window.extract_variables()["project_path"]
+        project_path = window.extract_variables()["project_path"]
+
         s = settings.get("output_directories")
         self.component_output_dir = os.path.join(project_path, s["components"])
+        self.system_output_dir = os.path.join(project_path, s["systems"])
 
-        self.window.show_input_panel("Name", "", self.on_done, None, None)
+        window.show_input_panel("Name", "", self.component_on_done, None, None)
 
-    def on_done(self, input):
+    def component_on_done(self, input):
         window = self.window
 
         filename = "{0}.{1}".format(input, settings.get("language"))
         output_file = os.path.join(self.component_output_dir, filename)
 
         if os.path.isfile(output_file):
-            status = "Component file already exists!"
+            status = "File already exists!"
             window.active_view().set_status("concord-helper", status)
             return
 
+        data = {
+            "components_path": settings.get("project_concord_path"),
+            "component_name": input
+        }
+
+        if self.write_file(data, output_file):
+            window.open_file(output_file)
+        else:
+            status = "Failed to write to file!"
+            window.active_view().set_status("concord-helper", status)
+
+    def write_file(self, data, output_file):
         # Read template
         with open(self.component_template, 'r') as in_file:
             content = in_file.read()
 
             # Write template
             with open(output_file, 'w+') as out_file:
-                out_file.write(content)
-                window.open_file(output_file)
+                out_file.write(content.format(**data))
+                return True
+
+        return False
